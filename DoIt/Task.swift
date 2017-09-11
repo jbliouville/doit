87c76ca9db0b4d6b7a8074b4cec58d6be848483e
Jean-baptiste Liouville <jb.liouville@gmail.com>
2017-09-12 00:22:00 +0200

    jusqu'à leçon 4

---
+++ DoIt/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Jean-baptiste Liouville on 11/09/2017.
+//  Copyright © 2017 Jean-baptiste Liouville. All rights reserved.
+//
+
+import Foundation
+
+class Task{
+    var name = ""
+    var important = false
+}
