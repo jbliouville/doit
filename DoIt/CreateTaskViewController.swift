68871e565667a2769e844f41bcd29f2a7a550d09
Jean-baptiste Liouville <jb.liouville@gmail.com>
2017-09-12 08:32:41 +0200

    etape 6 finie

---
+++ DoIt/CreateTaskViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CreateTaskViewController.swift
+//  DoIt
+//
+//  Created by Jean-baptiste Liouville on 12/09/2017.
+//  Copyright © 2017 Jean-baptiste Liouville. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameTextField: UITextField!
+    
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    @IBAction func tapped(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
